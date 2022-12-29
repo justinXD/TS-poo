03914ad471377f6bb1ffdd146be5a324153beeb7
Justin D <isaac_justin@hotmail.com>
2022-12-29 12:28:04 -0600

    protected with polimorphism

---
+++ src/09-proteced.ts
@@ -0,0 +1,40 @@
+export class Animal {
+  constructor(
+    protected name: string  // es similar al private con la diferencia de que puedo usar herencia con protected
+    ) {}
+
+    move() {
+      console.log('moving along')
+    }
+
+    greeting(): string {
+      return `Hello I'm ${this.name}`
+    }
+
+    protected doSomething() {
+      console.log('something');
+    }
+}
+
+export class Cat extends Animal {
+  constructor(public name: string, public owner: string) {
+    super(name) // super hace referencia al constructor de la clase padre (Animal) para pasarle el parametro que necesita (name)
+  }
+  miaw(times: number) {
+    for (let index = 0; index < times; index++) {
+      console.log('miaw')
+    }
+    this.doSomething()
+  }
+
+  move() {  // esto es polimorfismo
+    console.log('do it like a cat')
+    super.move()
+  }
+}
+
+const gatihija = new Cat('gatihija', 'Justin');
+gatihija.move()
+console.log(gatihija.greeting())
+gatihija.miaw(1)
+console.log(gatihija.owner)
