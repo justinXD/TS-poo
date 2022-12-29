adb1320f6e33f28166b7f6f5d35f4c36bc685dcf
Justin D <isaac_justin@hotmail.com>
2022-12-29 13:56:20 -0600

    abtract classes

---
+++ src/12-abstract.ts
@@ -0,0 +1,6 @@
+import { Animal, Cat } from "./09-proteced";
+
+// const animal = new Animal('papalote');   // no se pueden crear instancias de una clase abstracta
+// animal.greeting()
+
+const copito = new Cat('copito', 'Justin');
